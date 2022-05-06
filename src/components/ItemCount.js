7b84986e2b9c21788d1ec94b32b281bb279a7f97
Andrea Vanina Rolon <andreavarolon@gmail.com>
2022-05-06 19:14:17 -0300

    se realizan modificaciones en código

---
+++ src/components/ItemCount.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import itemImg from "../images/monitor.jpg";
+import "./ItemCount.css";
+
+function ItemCount({ stock, initial, onAdd }) {
+  const [count, setCount] = useState(initial);
+
+  function add() {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  }
+
+  function rest() {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  }
+
+  function addCart() {
+    onAdd(count);
+  }
+
+  return (
+    <div className="itemCount">
+      <img className="itemCount__img" src={itemImg} alt="" />
+      <div className="itemCount__descrip">
+        <h2 className="itemCount__tit">Monitores</h2>
+        <p>Ver detalle</p>
+        <p>Unidades: {count}</p>
+        <button className="itemCount__lessBtnAdd" onClick={add}>
+          Agregar
+        </button>
+        <button className="itemCount__lessBtnRest" onClick={rest}>
+          Quitar
+        </button>
+        <button className="itemCount__lessBtnCart" onClick={addCart}>
+          Comprar
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,18 @@
+import ItemCount from "./ItemCount";
+
 function ItemListContainer({ greeting }) {
   return (
     <div className="itemListContainer">
-      <h1 className="itemListContainer__title" style={{ color: "#ffc107" }}>
+      <h1 className="itemListContainer__title" style={{ color: "#eccb00" }}>
         {greeting}
       </h1>
+      <ItemCount
+        stock={10}
+        initial={1}
+        onAdd={(count) =>
+          console.log(`Agregaron ${count} unidad/es al carrito`)
+        }
+      />
     </div>
   );
 }
